8bdf4a55c6492b9ed16538ef07a1864ec3c57fc7
Angela Weber <angelaweber95@hotmail.com>
2021-05-07 09:52:30 -0700

    add a couple styles and test array to see scores

--- components/ScoreBoard.js
+++ components/ScoreBoard.js
@@ -1,17 +1,49 @@
 import React, {useState} from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 export default function ScoreBoard(props) {
+    let testArray = [
+        {
+            round: 1,
+            score1: 10,
+        },
+        {
+            round: 2,
+            score1: 50,
+        },
+        {
+            round: 3,
+            score1: 30,
+        }
+    ]
     return (
       <div>
-          {/* <h1>{props.player1Name}</h1>
-          <h1>{props.player2Name}</h1> */}
           <Container>
+              <div >
+                <h1>Score Board</h1>
+              </div>
             <Row>
-                <Col>{props.player1Name}</Col>
+                <Col>
+                    {props.player1Name}
+                    {testArray.map((score) => {
+                        return (
+                            <div key={score.round} style={divStyle}>
+                                <p>{score.round})</p>
+                                <p>{score.score1}</p>
+                            </div>
+                        )
+                    })}
+                    
+                </Col>
                 <Col>{props.player2Name}</Col>
             </Row>
           </Container>
       </div>
     );
+  }
+
+  const divStyle = {
+    display: 'flex',
+    justifyContent: 'space-evenly'
   }
