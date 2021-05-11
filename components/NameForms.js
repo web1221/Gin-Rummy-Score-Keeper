5adf97f47835f6eb75a66d893e00515e3f989e6c
Angela Weber <angelaweber95@hotmail.com>
2021-05-11 14:04:22 -0700

    add class to form group

--- components/NameForms.js
+++ components/NameForms.js
@@ -13,15 +13,14 @@ export default function NameForms(props) {
       <div className={styles.container}>
         <header><h1>Welcome to the Gin Rummy Counter!</h1></header>
             <h3>Enter the Names of Players</h3> 
-            
             <Form>
-                <Form.Group as={Row}>
+                <Form.Group className={styles.formGroup} as={Row}>
                     <Form.Label column sm="2">Player 1</Form.Label>
                     <Col sm="10">
                         <Form.Control onChange={(event) => props.setPlayer1Name(event.target.value)} type="text" placeholder="Enter name" />
                     </Col>
                 </Form.Group>
-                <Form.Group as={Row}>
+                <Form.Group className={styles.formGroup} as={Row}>
                     <Form.Label column sm="2">Player 2</Form.Label>
                     <Col sm="10">
                         <Form.Control onChange={(event) => props.setPlayer2Name(event.target.value)} type="text" placeholder="Enter name" />
