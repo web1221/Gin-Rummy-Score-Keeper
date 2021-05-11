7b59616f5c55414e933aa71358ab2f8656a9144b
Angela Weber <angelaweber95@hotmail.com>
2021-05-11 13:57:42 -0700

    updating forms to have bootstrap from component

--- components/NameForms.js
+++ components/NameForms.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import styles from '../styles/NameForms.module.css'
+import {Container, Form, Button, Row, Col} from 'react-bootstrap';
+
 
 export default function NameForms(props) {
     const onSubmit = (event) => {
@@ -7,22 +9,29 @@ export default function NameForms(props) {
         props.setShowNameInputs(false)
     }
     return (
+    <Container>
       <div className={styles.container}>
         <header><h1>Welcome to the Gin Rummy Counter!</h1></header>
-        <section>
-            <h3>Enter the Names of Players</h3>
-            <form className={styles.formContainer}>
-                <label className={styles.label}>
-                    Player 1:
-                    <input type="text" name="name" onChange={(event) => props.setPlayer1Name(event.target.value)}/>
-                </label>
-                <label className={styles.label}>
-                    Player 2:
-                    <input onChange={(event) => props.setPlayer2Name(event.target.value)} type="text" name="name" />
-                </label>
-                <button onClick={onSubmit} type='submit'>Let Play!</button>
-            </form>
-        </section>
-      </div>
+            <h3>Enter the Names of Players</h3> 
+            
+            <Form>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="2">Player 1</Form.Label>
+                    <Col sm="10">
+                        <Form.Control onChange={(event) => props.setPlayer1Name(event.target.value)} type="text" placeholder="Enter name" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="2">Player 2</Form.Label>
+                    <Col sm="10">
+                        <Form.Control onChange={(event) => props.setPlayer2Name(event.target.value)} type="text" placeholder="Enter name" />
+                    </Col>
+                </Form.Group>
+                <Button onClick={onSubmit} variant="primary" type="submit">
+                    Let's Play!
+                </Button>
+            </Form>
+            </div>
+    </Container>
     );
   }
