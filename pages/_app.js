de5db85a8e6fd8faa403d3348f20c388a6d876d8
Angela Weber <angelaweber95@hotmail.com>
2021-05-11 19:21:22 -0700

    add google fonts to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./_app.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
