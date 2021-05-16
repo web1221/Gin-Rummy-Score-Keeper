461942b19e52a0eab7472abe9c8113d4ff673492
Angela Weber <angelaweber95@hotmail.com>
2021-05-16 13:29:37 -0700

    add reset for form and scores on click

--- components/ScoreBoard.js
+++ components/ScoreBoard.js
@@ -4,17 +4,20 @@ import styles from '../styles/ScoreBoard.module.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function ScoreBoard(props) {
-    const [round, setRound] = useState();
+    const [round, setRound] = useState(1);
     const [score1, setScore1] = useState(0);
     const [score2, setScore2] = useState(0);
     const [roundAndScore, setRoundAndScore] = useState([]);
 
     const onFormSubmit = () => {
+        setRound(round + 1)
         roundAndScore.push({
             round: round,
             score1: score1,
             score2: score2 
         });
+        document.getElementById("formId").reset();
+        addTotalScore()
     }
     let testArray = [
         {
@@ -38,23 +41,10 @@ export default function ScoreBoard(props) {
             score2: 100
         }
     ]
-    
-    useEffect(() => {
-        addTotalScore()
-    }, [props.player1TotalScore])
-
-    const addRoundsAndPoints = () => {
-        // Need to find way to push up correct round 
-        // How do we make the round increase? 
-        // Do you need to add a round state? 
-        // round, setRound = useState(0) round + 1 every time this runs.
-        // might think of adding addTotalScore to this function too? 
-        roundAndScoreObj.push({round: 3, score1: 10, score2: 40})
-    }
 
     const addTotalScore = () => {
-         props.setPlayer1TotalScore(testArray.reduce((accum,item) => accum + item.score1, 0))
-         props.setPlayer2TotalScore(testArray.reduce((accum,item) => accum + item.score2, 0))
+         props.setPlayer1TotalScore(roundAndScore.reduce((accum,item) => accum + item.score1, 0))
+         props.setPlayer2TotalScore(roundAndScore.reduce((accum,item) => accum + item.score2, 0))
     }
 
     return (
@@ -63,57 +53,72 @@ export default function ScoreBoard(props) {
               <div className={styles.header}>
                 <h1>Score Board</h1>
               </div>
-            <Table striped bordered variant="dark" size="lg">
-                <thead className={styles.tableHeader} key={30}>
-                    <tr>
-                    <th>Round</th>
-                    <th>{props.player1Name}</th>
-                    <th>{props.player2Name}</th>
-                    </tr>
-                </thead>
-       
-                <tbody>
-                {testArray.map((score) => {
+            <div style={{overflow: "scroll"}}>
+                <Table striped bordered variant="dark" size="lg">
+                    <thead className={styles.tableHeader} key={30}>
+                        <tr>
+                        <th>Round</th>
+                        <th>{props.player1Name}</th>
+                        <th>{props.player2Name}</th>
+                        </tr>
+                    </thead>
+        
+                    <tbody>
+                    {roundAndScore.map((score) => {
                         return (
                             <>
-                                <tr key={score.round}>
-                                <td>{score.round}</td>
-                                <td>{score.score1}</td>
-                                <td>{score.score2}</td>
-                                </tr>
-                             </>
-                        )
-                    })}
-                    <tr>
-                        <td>
-                            Total
-                        </td>
-                        <td>
-                            {props.player1TotalScore}
-                        </td>
-                        <td>
-                            {props.player2TotalScore}
-                        </td>
-                    </tr>
-                    </tbody>
+                                    <tr key={score.round}>
+                                    <td>{score.round}</td>
+                                    <td>{score.score1}</td>
+                                    <td>{score.score2}</td>
+                                    </tr>
+                                </>
+                            )
+                        })}
+                        <tr>
+                            <td>
+                                Total
+                            </td>
+                            <td>
+                                {props.player1TotalScore}
+                            </td>
+                            <td>
+                                {props.player2TotalScore}
+                            </td>
+                        </tr>
+                        </tbody>
 
-            </Table>
-            <Form>
+                </Table>
+            </div>
+            <Form id="formId">
                 <Form.Group className={styles.formGroup} as={Row}>
                     <Form.Label className={styles.labelText} column sm="2">Enter {props.player1Name}'s Score</Form.Label>
                     <Col sm="10">
-                        <Form.Control type="number" placeholder="Enter Score" />
+                        <Form.Control 
+                        
+                            onChange={(event) => setScore1(parseInt(event.target.value))} 
+                            type="number" 
+                            placeholder="Enter Score" 
+                        />
                     </Col>
                 </Form.Group>
                 <Form.Group className={styles.formGroup} as={Row}>
                     <Form.Label className={styles.labelText} column sm="2">Enter {props.player2Name}'s Score</Form.Label>
                     <Col sm="10">
-                        <Form.Control type="number" placeholder="Enter Score" />
+                        <Form.Control 
+                            onChange={(event) => setScore2(parseInt(event.target.value))} 
+                            type="number" 
+                            placeholder="Enter Score" 
+                        />
                     </Col>
                 </Form.Group>
                 <div className={styles.buttonContainer}>
 
-                <Button className={styles.buttonStyle}  variant="primary" type="submit">
+                <Button   
+                    onClick={() => onFormSubmit()}
+                    className={styles.buttonStyle}  
+                    variant="primary"
+                >
                     Enter Score
                 </Button>
                 </div>
